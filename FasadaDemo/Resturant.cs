6797dbdfa9d11cf3c428680c0b65b56e741906e9
MateuszOkrasak <112722880+MateuszOkrasak@users.noreply.github.com>
2022-10-22 13:53:19 +0200

    Fasad method

---
+++ FasadaDemo/Resturant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FasadaDemo
+{
+    internal class Resturant
+    {
+        private int ingredients(quest)
+        public double EstimateCost(int guest)
+        {
+            return guest * 100;
+
+        }
+        public void BuyIngredients(int quest)
+        {
+
+        }
+    }
+}
